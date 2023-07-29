8329471546e50de1c521f332cad1e26313987d53
Samira <abdullahsiam009@gmail.com>
2023-07-30 05:15:11 +0600

    User dashboard post created form ui

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -5,12 +5,28 @@ import connect from "@/utils/db";
 import { NextResponse } from "next/server";
 
 export const GET = async (req: any) => {
-  // fetch
+  const url = new URL(req.url);
+  const username = url.searchParams.get("username");
 
+  // fetch
   try {
     await connect();
-    const posts = await Post.find();
+    const posts = await Post.find(username && { username });
     return new NextResponse(JSON.stringify(posts) as any, { status: 200 });
+  } catch (err) {
+    return new NextResponse("Database Error", { status: 500 });
+  }
+};
+
+export const POST = async (req: any) => {
+  const body = await req.json();
+  console;
+  const newPost = new Post(body);
+  // fetch
+  try {
+    await connect();
+    await newPost.save();
+    return new NextResponse("Post has been created", { status: 200 });
   } catch (err) {
     return new NextResponse("Database Error", { status: 500 });
   }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import axios from "axios";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import useSWR from "swr";
 
 const Dashboard = () => {
   const router = useRouter();
   const session = useSession();
-  console.log(session);
+  const fetcher = (...args: any[]) => fetch(...args).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(
+    `/api/posts?username=${session?.data?.user?.name}`,
+    fetcher
+  );
+
   if (session.status === "loading") {
     return <p>loading..</p>;
   }
@@ -15,8 +23,64 @@ const Dashboard = () => {
     router.push("/dashboard/login");
   }
 
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const title = e.target[0].value;
+    const desc = e.target[1].value;
+    const img = e.target[2].value;
+    const content = e.target[3].value;
+
+    try {
+      await fetch("/api/posts/", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          desc,
+          image: img,
+          content,
+          username: session.data.user.name,
+        }),
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   if (session.status === "authenticated") {
-    return <div>Dashboard</div>;
+    return (
+      <div>
+        <div>
+          {data?.length > 0 ? (
+            data?.map((post) => (
+              <div>
+                <div>
+                  <img
+                    src={post.image}
+                    alt={post.title}
+                    width={400}
+                    height={250}
+                    className="object-contain"
+                  />
+                </div>
+                <h2>Post title</h2>
+                <span> ❌ </span>
+              </div>
+            ))
+          ) : (
+            <p>No post to show</p>
+          )}
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Title" />
+          <input type="text" placeholder="Description" />
+          <input type="text" placeholder="Image" />
+          <textarea placeholder="Content"></textarea>
+
+          <button>Send</button>
+        </form>
+      </div>
+    );
   }
 };
 
